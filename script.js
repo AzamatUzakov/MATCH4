b57257bcf5cd2aa5be23ef37750a231942162e98
AzamatUzakov <123230902+AzamatUzakov@users.noreply.github.com>
2023-01-24 15:02:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+
+
+let a = +prompt('a');
+let m = prompt('+ - * /');
+let b = +prompt('c');
+
+
+
+if (m === "+") {
+    alert( a + b) 
+} else if(m === "-") {
+    alert( a - b)
+}else if (m=== "/") {
+    alert ( a / b)
+}else if (m=== "%") {
+    alert( a % b)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+let a = prompt('первое число');
+a = Number(a);
+
+let m = prompt('оператор (/,+,*,-)');
+
+let b = prompt('второе число');
+b = Number(b);
+
+
+
+
+switch (m) {
+    case '+':
+        alert(a + b);
+        break;
+    case '-':
+        alert(a - b);
+        break;
+    case '/':
+        alert(a / b);
+        break;
+    case '*':
+        alert(a * b);
+}
+ */
